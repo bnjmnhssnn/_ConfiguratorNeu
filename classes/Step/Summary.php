ced09c8fd23b247d65e4215540410e5b27b71d21
bnjmnhssnn <info@benjamin-hosseinian.de>
2021-02-21 18:51:50 +0000

    Summary Schritt fertig

--- classes/Step/Summary.php
+++ classes/Step/Summary.php
@@ -8,8 +8,12 @@ class Summary extends AbstractStep implements StepInterface
     public function setup(array $current_selection) : void
     {
         $this->vars['template'] = 'step_summary';
-        $this->vars['title'] = 'Zusammenfassung & bestätigen';
-        $this->vars['paragraph'] = 'Lorem ispum dolor sit amet';
+        $this->vars['title'] = $this->config['step_summary_title'];
+        $this->vars['paragraph'] = $this->config['step_summary_paragraph'];
+        $this->vars['email_label'] = $this->config['step_summary_email_label'];
+        $this->vars['contact_label'] = $this->config['step_summary_contact_label'];
+        $this->vars['schoolname_label'] = $this->config['step_summary_schoolname_label'];
+        $this->vars['freetext_label'] = $this->config['step_summary_freetext_label'];  
     }
 
     public function confirm(array $post_vars, array $configurator_selection) : bool
@@ -20,21 +24,22 @@ class Summary extends AbstractStep implements StepInterface
             );
         }
         if(empty($post_vars['email'])) {
-            $this->error = 'Missing required field \'email\''; 
+            $this->error = $this->config['step_summary_email_error'] ?? 'Missing required field \'email\''; 
             return false; 
         }
-        if(empty($post_vars['name'])) {
-            $this->error = 'Missing required field \'name\''; 
+        if(empty($post_vars['contact'])) {
+            $this->error = $this->config['step_summary_contact_error'] ?? 'Missing required field \'contact\''; 
             return false; 
         }
         if(empty($post_vars['school_name'])) {
-            $this->error = 'Missing required field \'school_name\''; 
+            $this->error = $this->config['step_summary_schoolname_error'] ?? 'Missing required field \'school_name\''; 
             return false; 
         }
         $this->user_input = [
             'email' => $post_vars['email'],
             'name' => $post_vars['name'],
             'school_name' => $post_vars['school_name'],
+            'free_text' => $post_vars['free_text'] ?? ''
         ];
         return true;
     }
